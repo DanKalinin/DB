0a84c923e83967374b6a6e1c47f85e43c3d4994c
Dan Kalinin <daniil551@hotmail.com>
2017-07-18 23:59:30 +0300

    doc

--- Framework/Framework/FRCTVC.h
+++ Framework/Framework/FRCTVC.h
@@ -13,18 +13,18 @@
 
 @interface FRCTVC : UITableViewController <NSFetchedResultsControllerDelegate>
 
-@property IBInspectable UITableViewRowAnimation rowAnimation;
+@property IBInspectable UITableViewRowAnimation rowAnimation; // Row animation for content changes
 
-@property IBInspectable NSString *orderKeyPath;
+@property IBInspectable NSString *orderKeyPath; // Key path of object's numeric property which defines it's order in table view. The default value is @"order".
 @property IBInspectable BOOL orderInSection;
 
 @property NSFetchedResultsController *frc;
 
-@property NSManagedObject *object;
-@property NSSet<NSManagedObject *> *objects;
+@property NSManagedObject *object; // Object which cell is selected on appearance with scrolling to central position
+@property NSSet<NSManagedObject *> *objects; // Objects which cells are selected on appearance without scrolling
 
-- (NSString *)cellIdentifierForIndexPath:(NSIndexPath *)indexPath;
-- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
+- (NSString *)cellIdentifierForIndexPath:(NSIndexPath *)indexPath; // Reusable cell identifier at specified index path. The default value is @"Cell". Override to specify different value.
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath; // Override to configure the cell instead of - tableView:cellForRowAtIndexPath:
 
 - (void)prepareForReloadData;
 - (void)reloadData;
