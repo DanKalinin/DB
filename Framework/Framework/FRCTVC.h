15fcab9682d1fc585912f822cffd8dd6e12afcbc
Dan Kalinin <daniil551@hotmail.com>
2017-07-19 13:22:21 +0300

    doc

--- Framework/Framework/FRCTVC.h
+++ Framework/Framework/FRCTVC.h
@@ -15,10 +15,10 @@
 
 @property IBInspectable UITableViewRowAnimation rowAnimation; // Row animation for content changes
 
-@property IBInspectable NSString *orderKeyPath; // Key path of object's numeric property which defines it's order in table view. The default value is @"order".
-@property IBInspectable BOOL orderInSection;
+@property IBInspectable NSString *orderKeyPath; // Key path for numeric property of the object which defines it's order in table view. The default value is @"order".
+@property IBInspectable BOOL orderInSection; // YES - recompute the order when moving row within section. NO - across the table.
 
-@property NSFetchedResultsController *frc;
+@property NSFetchedResultsController *frc; // Controller maintaining the fetch request. Initialize, set delegate and perform the fetch in overriden - viewDidLoad method.
 
 @property NSManagedObject *object; // Object which cell is selected on appearance with scrolling to central position
 @property NSSet<NSManagedObject *> *objects; // Objects which cells are selected on appearance without scrolling
@@ -26,7 +26,7 @@
 - (NSString *)cellIdentifierForIndexPath:(NSIndexPath *)indexPath; // Reusable cell identifier at specified index path. The default value is @"Cell". Override to specify different value.
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath; // Override to configure the cell instead of - tableView:cellForRowAtIndexPath:
 
-- (void)prepareForReloadData;
-- (void)reloadData;
+- (void)prepareForReloadData; // Pause to deliver content changes setting @ frc delegate to nil
+- (void)reloadData; // Perform the fetch, reload the table view and restore @ frc delegate
 
 @end
